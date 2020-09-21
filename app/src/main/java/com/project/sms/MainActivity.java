1fbf7f99390367a000b789a631b83bdca39f695e
robertkadikilo <robert@barriowatch.com>
2020-09-21 19:13:21 +0300

    Comments Added

--- app/src/main/java/com/project/sms/MainActivity.java
+++ app/src/main/java/com/project/sms/MainActivity.java
@@ -1,11 +1,11 @@
 package com.project.sms;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -19,10 +19,12 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String phone = "255744463492",
-            name = "Hello Melk,,",
-            APIKey = "",
+    private String phone = "255743368863",
+            name = "Hello wORLD",
+            APIKey = "hgfdhhfjghjk/lk.jhbgdhnhgmfhmjksdfsdfsfsdasdfsdf",
             url =" https://portal.fullstackteamsix.com/api/v1/send/index.php";
+
+    TextView tv_response;
 
 
     @Override
@@ -30,33 +32,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+
+        tv_response = findViewById(R.id.tv_response);
+
+        //Set button on to execute send method when clicked
+        findViewById(R.id.btn_send_message).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                request_to_be_sent();
+                sendMessage();
             }
         });
     }
 
-    private void request_to_be_sent() {
+
+    //Method to send SMS
+    private void sendMessage() {
+
+        tv_response.setText("Sending ...");
 
 
-// Instantiate the RequestQueue.
+        // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
 
-// Request a string response from the provided URL.
+        // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
+
                         Log.d("Hello", response);
-                        // Display the first 500 characters of the response string.
-                        //textView.setText("Response is: "+ response.substring(0,500));
+                        tv_response.setText(response);
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                //textView.setText("That didn't work!");
+                tv_response.setText((CharSequence) error);
             }
         }) {
             @Override
@@ -71,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-// Add the request to the RequestQueue.
+        // Add the request to the RequestQueue.
         queue.add(stringRequest);
     }
 }
